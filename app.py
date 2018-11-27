63951061e68bdbfb6e02f44d7c900cf5d22d28e6
ABHINAV VAJPEYI <abhinav@paralleldots.com>
2018-11-28 01:05:13 +0530

    changes

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from flask import Flask, request
 import db
 import apis
+from flask import Flask,request,render_template
 app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
 
 @app.route('/serve', methods=['GET', 'POST'])
 def serve():
-    code = request.values['code']
+    code = request.form['code']
     output = apis.serve_frames(code)
     return output
 
 @app.route('/details', methods=['GET', 'POST'])
 def details():
-    name=request.values['name']
-    city=request.values['city']
+    name=request.form['name']
+    city=request.form['city']
+
     output=apis.serve_request(name,city)
     return output
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
